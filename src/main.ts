42d678310eb29404a3a102dd6623b0108804a189
herman <hermanconnor11@gmail.com>
2022-12-02 00:13:04 -0500

    Add Lato font

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,18 @@ const initApp = () => {
         count = 0;
       }
 
+      if (count > 0) {
+        value.style.color = '#22c55e';
+      }
+
+      if (count < 0) {
+        value.style.color = '#ef4444';
+      }
+
+      if (count === 0) {
+        value.style.color = '#262626';
+      }
+
       value.innerText = count.toString();
     });
   });
